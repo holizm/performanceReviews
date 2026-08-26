c2d40448873af5b2c285df62006d41f2436561d0
Saeed Nemati <sn.1361@gmail.com>
2026-08-26 10:28:59 +0330

    committed by script

---
+++ panel/admin/breadcrumb.jsx
@@ -0,0 +1,8 @@
+import PartBreadcrumb from 'partBreadcrumb'
+
+export default props => <PartBreadcrumb
+    icon='360'
+    name='performanceReviews'
+    title='performanceReviewsPerformanceReviews'
+    {...props}
+/>
